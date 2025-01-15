cd4e027aabb395774a1a581093593e74476518b5
dima <denmi104@gmail.com>
2025-01-15 19:42:17 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 9;
+console.log(number);
+
+const obj = {
+    'one': 25,
+    'two': 31
+};
+console.log(obj);
+// alert('hello, you are super)');   всплывающее окно, предупреждение
+
+// const result = confirm('Are you here?');   всплывающее окно с вопросом и возможностью ответа (да/нет)
+// const answer = prompt('vam est` 18?', '18');  всплывающее окно с возможностью полноценного ответа.
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('name', '');
+// answers[1] = prompt('age', '');
+// answers[2] = prompt('adress', '');
+// document.write(answers);
+// console.log(answers);
+
+const user = 'Dima';
+alert(`Hello, ${user}`);
