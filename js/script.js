2ef9d2c655740b2ee5e6c3dc0f830838575beb7f
Dima <denmi104@gmail.com>
2025-01-17 14:01:44 +0200

    continius...

--- js/script.js
+++ js/script.js
@@ -26,18 +26,45 @@
 
 // const user = 'Dima';
 // alert(`Hello, ${user}`);
-const numberOfFolms = +prompt('Сколько фильмов вы уже посмотрели?', "");
-const personalMovieDB = {
-    count: numberOfFolms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+// const numberOfFolms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+// const personalMovieDB = {
+//     count: numberOfFolms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', '');
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+
+
+
+// =======================================================================
+
+
+const num = 50;
+switch (num) {
+    case 49:
+        console.log('error');
+        break;
+    case 100:
+        console.log('ne verno');
+        break;
+    case 51:
+        console.log('v tochku');
+        break;
+    default:
+        console.log('ne v etot raz!');
+}
+
+const test = 19;
+if (test < 20) {
+    console.log('net');
+} else if (test === 20) {
+    console.log('otlichno!');
+}
