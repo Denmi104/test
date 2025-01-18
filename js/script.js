2744df940a19d37eee7b7faf68283b7693500c0a
Dima <denmi104@gmail.com>
2025-01-18 21:47:06 +0200

    saturday

--- js/script.js
+++ js/script.js
@@ -73,28 +73,28 @@
 // }
 
 // ========================================================
-console.log( NaN || 2 || undefined );
+// console.log( NaN || 2 || undefined );
 
 
-console.log( NaN && 2 && undefined );
+// console.log( NaN && 2 && undefined );
 
 
-console.log( 1 && 2 && 3 );
+// console.log( 1 && 2 && 3 );
 
 
-console.log( !1 && 2 || !3 );
+// console.log( !1 && 2 || !3 );
 
 
-console.log( 25 || null && !3 );
+// console.log( 25 || null && !3 );
 
 
-console.log( NaN || null || !3 || undefined || 5);
+// console.log( NaN || null || !3 || undefined || 5);
 
 
-console.log( NaN || null && !3 && undefined || 5);
+// console.log( NaN || null && !3 && undefined || 5);
 
 
-console.log(5 === 5 && 3 > 1 || 5);
+// console.log(5 === 5 && 3 > 1 || 5);
 
 // const hamburger = 3;
 // const fries = 3;
@@ -158,8 +158,62 @@ console.log(5 === 5 && 3 > 1 || 5);
 // }
 
 
-for (let i = 1; i < 11; i++){
-    if (i % 2==0) {
-        console.log(i);
+// for (let i = 1; i < 11; i++){
+//     if (i % 2==0) {
+//         console.log(i);
+//     }
+// }
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
+
+//     // Пишем решение вот тут
+//     for (let i = 0; i<arr.length;i++){
+//         result[i]=arr[i];
+// }
+// console.log(result);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// for (let i = 0; i < data.length; i++) {
+//     if (typeof (data[i]) === 'number') {
+//         data[i] = data[i] * 2;
+//     } else if (typeof (data[i]) === 'string') {
+//     data[i] = `${data[i]} - done`;
+// }
+// }
+    
+// console.log(data);
+
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//     for (let i = 0; i < data.length; i++) {
+//         if (typeof(data[i]) === 'number') {
+//             data[i] = data[i] * 2;
+//         } else if (typeof(data[i]) === 'string') {
+//             data[i] = `${data[i]} - done`;
+//         }
+//     }
+
+// console.log(data);
+    
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [data.reverse()];
+// console.log(result);
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
     }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
 }
+
+console.log(result)
